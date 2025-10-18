2a4a808985bf65167acf81b944320e3fede4066e
Damontecres <damontecres@gmail.com>
2025-10-18 15:37:17 -0400

    Add logcat

--- app/src/main/java/com/github/damontecres/wholphin/ui/detail/DebugPage.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/detail/DebugPage.kt
@@ -1,5 +1,6 @@
 package com.github.damontecres.wholphin.ui.detail
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.scrollBy
@@ -14,6 +15,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
@@ -37,6 +39,8 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import javax.inject.Inject
 
 @HiltViewModel
@@ -47,6 +51,7 @@ class DebugViewModel
         val itemPlaybackDao: ItemPlaybackDao,
     ) : ViewModel() {
         val itemPlaybacks = MutableLiveData<List<ItemPlayback>>(listOf())
+        val logcat = MutableLiveData<List<LogcatLine>>(listOf())
 
         init {
             viewModelScope.launchIO {
@@ -55,10 +60,61 @@ class DebugViewModel
                     withContext(Dispatchers.Main) {
                         itemPlaybacks.value = results
                     }
+                    val logcat = getLogCatLines()
+                    withContext(Dispatchers.Main) {
+                        this@DebugViewModel.logcat.value = logcat
+                    }
                 }
             }
         }
+
+        fun getLogCatLines(): List<LogcatLine> {
+            val lineCount = 500
+            val args =
+                buildList {
+                    add("logcat")
+                    add("-d")
+                    add("-t")
+                    add(lineCount.toString())
+                    addAll(THIRD_PARTY_TAGS)
+                    add("*:V")
+                }
+            val process = ProcessBuilder().command(args).redirectErrorStream(true).start()
+            val logLines = mutableListOf<LogcatLine>()
+            try {
+                val reader = BufferedReader(InputStreamReader(process.inputStream))
+                var count = 0
+
+                while (count < lineCount) {
+                    val line = reader.readLine()
+                    if (line != null) {
+                        val level = line.split(" ").getOrNull(4)
+                        val logLevel =
+                            when (level?.uppercase()) {
+                                "V" -> Log.VERBOSE
+                                "D" -> Log.DEBUG
+                                "I" -> Log.INFO
+                                "W" -> Log.WARN
+                                "E" -> Log.ERROR
+                                else -> Log.VERBOSE
+                            }
+                        logLines.add(LogcatLine(logLevel, line))
+                    } else {
+                        break
+                    }
+                    count++
+                }
+            } finally {
+                process.destroy()
+            }
+            return logLines
+        }
     }
+
+data class LogcatLine(
+    val level: Int,
+    val text: String,
+)
 
 @Composable
 fun DebugPage(
@@ -77,6 +133,7 @@ fun DebugPage(
     }
 
     val itemPlaybacks by viewModel.itemPlaybacks.observeAsState(listOf())
+    val logcat by viewModel.logcat.observeAsState(listOf())
 
     LazyColumn(
         state = columnState,
@@ -192,5 +249,62 @@ fun DebugPage(
                 }
             }
         }
+        item {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Logcat",
+                    style = MaterialTheme.typography.displaySmall,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+                logcat.forEach { (level, line) ->
+                    val color =
+                        when (level) {
+                            Log.VERBOSE -> MaterialTheme.colorScheme.onSurface
+                            Log.DEBUG -> Color(0xff2bc4cf)
+                            Log.INFO -> Color(0xff2bcf8b)
+                            Log.WARN -> Color(0xffdde663)
+                            Log.ERROR -> Color(0xffe67063)
+                            else -> MaterialTheme.colorScheme.onSurface
+                        }
+                    Text(
+                        text = line,
+                        style = MaterialTheme.typography.bodySmall,
+                        color = color,
+                    )
+                }
+            }
+        }
     }
 }
+
+private val THIRD_PARTY_TAGS =
+    listOf(
+        "libc:F",
+        "ExoPlayerImpl:W",
+        // FireTV
+        "Codec2Client:E",
+        "CCodecBuffers:E",
+        "CCodecConfig:E",
+        "okhttp.Http2:W",
+        "okhttp.TaskRunner:W",
+        "LruBitmapPool:W",
+        "FragmentManager:W",
+        "ConfigStore:W",
+        "GlideRequest:W",
+        "FactoryPools:W",
+        "ViewTarget:W",
+        "Engine:W",
+        "Downsampler:W",
+        "TransformationUtils:W",
+        "DecodeJob:W",
+        "BufferPoolAccessor2.0:W",
+        "ExifInterface:W",
+        "MediaCodec:W",
+        "SurfaceUtils:W",
+        "ByteArrayPool:W",
+        "HardwareConfig:W",
+        "DfltImageHeaderParser:W",
+    )
