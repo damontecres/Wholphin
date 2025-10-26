5285c66036de08a761b93ea7c30c69d827fc88e7
Damontecres <damontecres@gmail.com>
2025-10-26 17:44:31 -0400

    Update aboutLibraries dependency

--- app/src/main/java/com/github/damontecres/wholphin/ui/components/LicenseInfo.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/components/LicenseInfo.kt
@@ -3,12 +3,12 @@ package com.github.damontecres.wholphin.ui.components
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.mikepenz.aboutlibraries.ui.compose.android.rememberLibraries
+import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 
 @Composable
 fun LicenseInfo(modifier: Modifier = Modifier) {
-    val libraries by rememberLibraries()
+    val libraries by produceLibraries()
 
     LibrariesContainer(libraries, modifier)
 }
