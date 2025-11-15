2f56a3a114e7322663cc1a18ee6a96c60f613b0b
damontecres <154766448+damontecres@users.noreply.github.com>
2025-11-14 19:48:47 -0500

    Add a few more sort options (#223)
    Adds parental rating, runtime, & play count sorting options
    
    Closes #198

--- app/src/main/java/com/github/damontecres/wholphin/ui/data/SortAndDirection.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/data/SortAndDirection.kt
@@ -30,6 +30,8 @@ val MovieSortOptions =
         ItemSortBy.DATE_PLAYED,
         ItemSortBy.COMMUNITY_RATING,
         ItemSortBy.CRITIC_RATING,
+        ItemSortBy.OFFICIAL_RATING,
+        ItemSortBy.RUNTIME,
         ItemSortBy.PLAY_COUNT,
         ItemSortBy.RANDOM,
     )
@@ -42,6 +44,7 @@ val SeriesSortOptions =
         ItemSortBy.DATE_LAST_CONTENT_ADDED,
         ItemSortBy.DATE_PLAYED,
         ItemSortBy.COMMUNITY_RATING,
+        ItemSortBy.OFFICIAL_RATING,
         ItemSortBy.RANDOM,
     )
 
@@ -51,6 +54,8 @@ val EpisodeSortOptions =
         ItemSortBy.DATE_CREATED,
         ItemSortBy.DATE_PLAYED,
         ItemSortBy.AIRED_EPISODE_ORDER,
+        ItemSortBy.OFFICIAL_RATING,
+        ItemSortBy.RUNTIME,
         ItemSortBy.RANDOM,
     )
 
@@ -59,6 +64,8 @@ val VideoSortOptions =
         ItemSortBy.SORT_NAME,
         ItemSortBy.DATE_CREATED,
         ItemSortBy.DATE_PLAYED,
+        ItemSortBy.RUNTIME,
+        ItemSortBy.PLAY_COUNT,
         ItemSortBy.RANDOM,
     )
 
@@ -68,17 +75,22 @@ fun getStringRes(sort: ItemSortBy): Int =
         ItemSortBy.SORT_NAME,
         ItemSortBy.SERIES_SORT_NAME,
         -> R.string.sort_by_name
+
         ItemSortBy.PREMIERE_DATE -> R.string.sort_by_date_released
         ItemSortBy.DATE_CREATED -> R.string.sort_by_date_added
         ItemSortBy.DATE_LAST_CONTENT_ADDED -> R.string.sort_by_date_episode_added
+
         ItemSortBy.DATE_PLAYED,
         ItemSortBy.SERIES_DATE_PLAYED,
         -> R.string.sort_by_date_played
+
         ItemSortBy.RANDOM -> R.string.sort_by_random
         ItemSortBy.COMMUNITY_RATING -> R.string.community_rating
         ItemSortBy.CRITIC_RATING -> R.string.critic_rating
+        ItemSortBy.OFFICIAL_RATING -> R.string.official_rating
         ItemSortBy.PLAY_COUNT -> R.string.play_count
         ItemSortBy.AIRED_EPISODE_ORDER -> R.string.aired_episode_order
+        ItemSortBy.RUNTIME -> R.string.runtime_sort
         ItemSortBy.DEFAULT -> R.string.default_track
         else -> throw IllegalArgumentException("Unsupported sort option: $sort")
     }
