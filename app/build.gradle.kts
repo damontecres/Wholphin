b78f5fcc77c44b82c4d08cb1ba8bd5b2769fc41f
Ray <154766448+damontecres@users.noreply.github.com>
2026-01-11 19:43:21 -0500

    Better support for remote media (HLS/DASH) (#675)
    ## Description
    Check if playback is for a remote source and, if so, play that remote
    URL directly. Works for both ExoPlayer & MPV.
    
    Also adds support for direct playback for DASH in ExoPlayer.
    
    ### Related issues
    Closes #471
    Closes #666

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -199,6 +199,7 @@ dependencies {
     implementation(libs.androidx.media3.session)
     implementation(libs.androidx.media3.datasource.okhttp)
     implementation(libs.androidx.media3.exoplayer.hls)
+    implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.ui)
     implementation(libs.androidx.media3.ui.compose)
 
