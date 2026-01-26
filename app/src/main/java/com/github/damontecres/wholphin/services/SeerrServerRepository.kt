05e18ec952f27d438d09a32754a5c43c4c0f7d1c
Damontecres <damontecres@gmail.com>
2026-01-26 16:27:28 -0500

    Try different url variations for seerr login

--- app/src/main/java/com/github/damontecres/wholphin/services/SeerrServerRepository.kt
+++ app/src/main/java/com/github/damontecres/wholphin/services/SeerrServerRepository.kt
@@ -30,6 +30,7 @@ import okhttp3.OkHttpClient
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * Manages saves/loading Seerr servers from the local DB. Also will update the current [SeerrApi] as needed.
@@ -138,7 +139,16 @@ class SeerrServerRepository
             passwordOrApiKey: String,
         ): LoadingState {
             val apiKey = passwordOrApiKey.takeIf { authMethod == SeerrAuthMethod.API_KEY }
-            val api = SeerrApiClient(url, apiKey, okHttpClient)
+            val api =
+                SeerrApiClient(
+                    url,
+                    apiKey,
+                    okHttpClient
+                        .newBuilder()
+                        .connectTimeout(5.seconds)
+                        .readTimeout(6.seconds)
+                        .build(),
+                )
             login(api, authMethod, username, passwordOrApiKey)
             return LoadingState.Success
         }
--- app/src/main/java/com/github/damontecres/wholphin/ui/setup/seerr/SwitchSeerrViewModel.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/setup/seerr/SwitchSeerrViewModel.kt
@@ -16,7 +16,8 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okhttp3.HttpUrl
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import timber.log.Timber
 
 @HiltViewModel
@@ -32,20 +33,6 @@ class SwitchSeerrViewModel
 
         val serverConnectionStatus = MutableStateFlow<LoadingState>(LoadingState.Pending)
 
-        private fun cleanUrl(url: String) =
-            if (!url.endsWith("/api/v1")) {
-                url
-                    .toHttpUrlOrNull()
-                    ?.newBuilder()
-                    ?.apply {
-                        addPathSegment("api")
-                        addPathSegment("v1")
-                    }?.build()
-                    .toString()
-            } else {
-                url
-            }
-
         fun submitServer(
             url: String,
             username: String,
@@ -53,45 +40,63 @@ class SwitchSeerrViewModel
             authMethod: SeerrAuthMethod,
         ) {
             viewModelScope.launchIO {
-                val url = cleanUrl(url)
-                val result =
+                serverConnectionStatus.update { LoadingState.Loading }
+                val urls =
                     try {
-                        seerrServerRepository.testConnection(
-                            authMethod = authMethod,
-                            url = url,
-                            username = username.takeIf { authMethod != SeerrAuthMethod.API_KEY },
-                            passwordOrApiKey = passwordOrApiKey,
-                        )
-                    } catch (ex: ClientException) {
-                        Timber.w(ex, "Error logging in via API Key")
-                        if (ex.statusCode == 401 || ex.statusCode == 403) {
-                            showToast(context, "Invalid credentials")
-                            LoadingState.Error("Invalid credentials", ex)
-                        } else {
-                            showToast(context, "Error: ${ex.localizedMessage}")
+                        createUrls(url)
+                    } catch (ex: IllegalArgumentException) {
+                        showToast(context, "Invalid URL")
+                        serverConnectionStatus.update { LoadingState.Error("Invalid URL", ex) }
+                        return@launchIO
+                    }
+                var result: LoadingState = LoadingState.Error("No url")
+                for (url in urls) {
+                    Timber.d("Trying %s", url)
+                    result =
+                        try {
+                            seerrServerRepository.testConnection(
+                                authMethod = authMethod,
+                                url = url.toString(),
+                                username = username.takeIf { authMethod != SeerrAuthMethod.API_KEY },
+                                passwordOrApiKey = passwordOrApiKey,
+                            )
+                        } catch (ex: ClientException) {
+                            Timber.w(ex, "Error logging in")
+                            if (ex.statusCode == 401 || ex.statusCode == 403) {
+                                showToast(context, "Invalid credentials")
+                                LoadingState.Error("Invalid credentials", ex)
+                                break
+                            } else {
+                                LoadingState.Error("Could not connect with URL")
+                            }
+                        } catch (ex: Exception) {
                             LoadingState.Error(ex)
                         }
-                    }
-                if (result is LoadingState.Success) {
-                    when (authMethod) {
-                        SeerrAuthMethod.LOCAL,
-                        SeerrAuthMethod.JELLYFIN,
-                        -> {
-                            seerrServerRepository.addAndChangeServer(
-                                url,
-                                authMethod,
-                                username,
-                                passwordOrApiKey,
-                            )
-                        }
+                    if (result is LoadingState.Success) {
+                        when (authMethod) {
+                            SeerrAuthMethod.LOCAL,
+                            SeerrAuthMethod.JELLYFIN,
+                            -> {
+                                seerrServerRepository.addAndChangeServer(
+                                    url.toString(),
+                                    authMethod,
+                                    username,
+                                    passwordOrApiKey,
+                                )
+                            }
 
-                        SeerrAuthMethod.API_KEY -> {
-                            seerrServerRepository.addAndChangeServer(
-                                url,
-                                passwordOrApiKey,
-                            )
+                            SeerrAuthMethod.API_KEY -> {
+                                seerrServerRepository.addAndChangeServer(
+                                    url.toString(),
+                                    passwordOrApiKey,
+                                )
+                            }
                         }
+                        break
                     }
+                }
+                if (result is LoadingState.Error) {
+                    showToast(context, "Error: ${result.message}")
                 }
                 serverConnectionStatus.update { result }
             }
@@ -106,4 +111,40 @@ class SwitchSeerrViewModel
         fun resetStatus() {
             serverConnectionStatus.update { LoadingState.Pending }
         }
+    }
+
+fun createUrls(url: String): List<HttpUrl> {
+    val urls = mutableListOf<String>()
+    if (url.startsWith("http://") || url.startsWith("https://")) {
+        urls.add(url)
+        val httpUrl = url.toHttpUrl()
+        if (HttpUrl.defaultPort(httpUrl.scheme) == httpUrl.port) {
+            urls.add("$url:5055")
+        }
+    } else {
+        urls.add("http://$url")
+        val httpUrl = "http://$url".toHttpUrl()
+        if (httpUrl.port == 80) {
+            urls.add("https://$url")
+            urls.add("http://$url:5055")
+            urls.add("https://$url:5055")
+        } else {
+            urls.add("https://$url")
+        }
+    }
+    return urls.map { cleanUrl(it).toHttpUrl() }
+}
+
+private fun cleanUrl(url: String) =
+    if (!url.endsWith("/api/v1")) {
+        url
+            .toHttpUrl()
+            .newBuilder()
+            .apply {
+                addPathSegment("api")
+                addPathSegment("v1")
+            }.build()
+            .toString()
+    } else {
+        url
     }
