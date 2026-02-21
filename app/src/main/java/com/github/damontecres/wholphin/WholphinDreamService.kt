876725f46d9562e6962c2314cbe1034fa9333137
Damontecres <damontecres@gmail.com>
2026-02-21 11:16:28 -0500

    Apply theme

--- app/src/main/java/com/github/damontecres/wholphin/WholphinDreamService.kt
+++ app/src/main/java/com/github/damontecres/wholphin/WholphinDreamService.kt
@@ -20,6 +20,7 @@ import com.github.damontecres.wholphin.preferences.UserPreferences
 import com.github.damontecres.wholphin.services.ScreensaverService
 import com.github.damontecres.wholphin.services.UserPreferencesService
 import com.github.damontecres.wholphin.ui.components.AppScreensaverContent
+import com.github.damontecres.wholphin.ui.theme.WholphinTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.milliseconds
@@ -59,18 +60,25 @@ class WholphinDreamService :
                 setViewTreeLifecycleOwner(this@WholphinDreamService)
                 setViewTreeSavedStateRegistryOwner(this@WholphinDreamService)
                 setContent {
-                    val prefs by userPreferencesService.flow.collectAsState(UserPreferences(AppPreferences.getDefaultInstance()))
-                    val screensaverPrefs = prefs.appPreferences.interfacePreferences.screensaverPreference
-                    val currentItem by itemFlow.collectAsState(null)
-                    currentItem?.let { currentItem ->
-                        key(currentItem) {
-                            AppScreensaverContent(
-                                currentItem = currentItem,
-                                showClock = prefs.appPreferences.interfacePreferences.showClock,
-                                duration = screensaverPrefs.duration.milliseconds,
-                                animate = screensaverPrefs.animate,
-                                modifier = Modifier.fillMaxSize(),
-                            )
+                    val prefs by userPreferencesService.flow.collectAsState(
+                        UserPreferences(
+                            AppPreferences.getDefaultInstance(),
+                        ),
+                    )
+                    WholphinTheme(appThemeColors = prefs.appPreferences.interfacePreferences.appThemeColors) {
+                        val screensaverPrefs =
+                            prefs.appPreferences.interfacePreferences.screensaverPreference
+                        val currentItem by itemFlow.collectAsState(null)
+                        currentItem?.let { currentItem ->
+                            key(currentItem) {
+                                AppScreensaverContent(
+                                    currentItem = currentItem,
+                                    showClock = prefs.appPreferences.interfacePreferences.showClock,
+                                    duration = screensaverPrefs.duration.milliseconds,
+                                    animate = screensaverPrefs.animate,
+                                    modifier = Modifier.fillMaxSize(),
+                                )
+                            }
                         }
                     }
                 }
