50e1e3e9a6ea446aef5fe6285b7bd992d5a936b4
Damontecres <damontecres@gmail.com>
2026-03-01 19:18:25 -0500

    Update delete dialog

--- app/src/main/java/com/github/damontecres/wholphin/ui/components/Dialogs.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/components/Dialogs.kt
@@ -19,11 +19,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -32,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
@@ -519,12 +523,72 @@ fun ConfirmDeleteDialog(
     onCancel: () -> Unit,
     onConfirm: () -> Unit,
 ) {
-    ConfirmDialog(
-        title = stringResource(R.string.delete),
-        body = stringResource(R.string.delete_item, itemTitle),
-        onCancel = onCancel,
-        onConfirm = onConfirm,
-    )
+    BasicDialog(
+        onDismissRequest = onCancel,
+        properties = DialogProperties(usePlatformDefaultWidth = false),
+    ) {
+        LazyColumn(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier.wrapContentWidth(),
+        ) {
+            item {
+                Text(
+                    text = stringResource(R.string.delete_item),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.onSurface,
+                )
+            }
+            item {
+                Text(
+                    text = itemTitle,
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    textAlign = TextAlign.Center,
+                    modifier =
+                        Modifier
+                            .padding(bottom = 8.dp),
+                )
+            }
+
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(32.dp),
+                    modifier = Modifier,
+                ) {
+                    Button(
+                        onClick = onCancel,
+                        modifier = Modifier.width(72.dp),
+                    ) {
+                        Text(
+                            text = stringResource(R.string.cancel),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.fillMaxWidth(),
+                        )
+                    }
+                    Button(
+                        onClick = onConfirm,
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.padding(horizontal = 4.dp),
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = null,
+                                tint = Color.Red.copy(alpha = .8f),
+                                modifier = Modifier,
+                            )
+                            Text(
+                                text = stringResource(R.string.confirm),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 fun chooseVersionParams(
