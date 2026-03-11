38589f0eb49474524f655bd2e802419cfdd0f21d
Damontecres <damontecres@gmail.com>
2026-03-11 17:55:38 -0400

    Use jellyfin images for available discovered items

--- app/src/main/java/com/github/damontecres/wholphin/services/SeerrService.kt
+++ app/src/main/java/com/github/damontecres/wholphin/services/SeerrService.kt
@@ -3,6 +3,7 @@ package com.github.damontecres.wholphin.services
 import com.github.damontecres.wholphin.api.seerr.SeerrApiClient
 import com.github.damontecres.wholphin.api.seerr.model.CreditCast
 import com.github.damontecres.wholphin.api.seerr.model.CreditCrew
+import com.github.damontecres.wholphin.api.seerr.model.MediaInfo
 import com.github.damontecres.wholphin.api.seerr.model.MovieDetails
 import com.github.damontecres.wholphin.api.seerr.model.MovieResult
 import com.github.damontecres.wholphin.api.seerr.model.SearchGet200ResponseResultsInner
@@ -12,6 +13,7 @@ import com.github.damontecres.wholphin.data.model.BaseItem
 import com.github.damontecres.wholphin.data.model.DiscoverItem
 import com.github.damontecres.wholphin.data.model.SeerrAvailability
 import com.github.damontecres.wholphin.data.model.SeerrItemType
+import com.github.damontecres.wholphin.ui.isNotNullOrBlank
 import com.github.damontecres.wholphin.ui.toLocalDate
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
@@ -34,6 +36,7 @@ class SeerrService
     constructor(
         private val seerApi: SeerrApi,
         private val seerrServerRepository: SeerrServerRepository,
+        private val imageUrlService: ImageUrlService,
     ) {
         val api: SeerrApiClient get() = seerApi.api
 
@@ -96,7 +99,6 @@ class SeerrService
                     ?.get("Tmdb")
                     ?.toIntOrNull()
                     ?.let {
-                        val current = seerrServerRepository.current.firstOrNull() ?: return@let null
                         when (item.type) {
                             BaseItemKind.MOVIE -> {
                                 api.moviesApi
@@ -151,9 +153,46 @@ class SeerrService
                 null
             }
 
-        suspend fun createDiscoverItem(movie: MovieResult): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        private suspend fun createImageUrl(
+            imageType: ImageType,
+            path: String?,
+            mediaInfo: MediaInfo?,
+        ): String? {
+            if (mediaInfo != null) {
+                val itemId =
+                    if (mediaInfo.jellyfinMediaId.isNotNullOrBlank()) {
+                        mediaInfo.jellyfinMediaId.toUUIDOrNull()
+                    } else if (mediaInfo.jellyfinMediaId4k.isNotNullOrBlank()) {
+                        mediaInfo.jellyfinMediaId4k.toUUIDOrNull()
+                    } else {
+                        null
+                    }
+                if (itemId != null) {
+                    return imageUrlService.getItemImageUrl(
+                        itemId = itemId,
+                        imageType = imageType,
+                    )
+                }
+            }
+            val current = seerrServerRepository.current.firstOrNull() ?: return null
+            val cacheImages = current.serverConfig.cacheImages == true
+            val base =
+                if (cacheImages) {
+                    current.server.url.removeSuffix("/") + "/imageproxy/tmdb"
+                } else {
+                    "https://image.tmdb.org"
+                }
+            val prefix =
+                when (imageType) {
+                    ImageType.PRIMARY -> "/t/p/w500"
+                    ImageType.BACKDROP -> "/t/p/w1920_and_h1080_multi_faces"
+                    else -> throw IllegalArgumentException("Image type not supported: $imageType")
+                }
+            return "${base}${prefix}$path"
+        }
+
+        suspend fun createDiscoverItem(movie: MovieResult): DiscoverItem =
+            DiscoverItem(
                 id = movie.id,
                 type = SeerrItemType.MOVIE,
                 title = movie.title,
@@ -163,15 +202,13 @@ class SeerrService
                     SeerrAvailability.from(movie.mediaInfo?.status)
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(movie.releaseDate),
-                posterUrl = current.imageUrlBuilder(ImageType.PRIMARY, movie.posterPath),
-                backDropUrl = current.imageUrlBuilder(ImageType.BACKDROP, movie.backdropPath),
+                posterUrl = createImageUrl(ImageType.PRIMARY, movie.posterPath, movie.mediaInfo),
+                backDropUrl = createImageUrl(ImageType.BACKDROP, movie.backdropPath, movie.mediaInfo),
                 jellyfinItemId = movie.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(movie: MovieDetails): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(movie: MovieDetails): DiscoverItem =
+            DiscoverItem(
                 id = movie.id ?: -1,
                 type = SeerrItemType.MOVIE,
                 title = movie.title,
@@ -181,15 +218,13 @@ class SeerrService
                     SeerrAvailability.from(movie.mediaInfo?.status)
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(movie.releaseDate),
-                posterUrl = current.imageUrlBuilder(ImageType.PRIMARY, movie.posterPath),
-                backDropUrl = current.imageUrlBuilder(ImageType.BACKDROP, movie.backdropPath),
+                posterUrl = createImageUrl(ImageType.PRIMARY, movie.posterPath, movie.mediaInfo),
+                backDropUrl = createImageUrl(ImageType.BACKDROP, movie.backdropPath, movie.mediaInfo),
                 jellyfinItemId = movie.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(tv: TvResult): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(tv: TvResult): DiscoverItem =
+            DiscoverItem(
                 id = tv.id!!,
                 type = SeerrItemType.TV,
                 title = tv.name,
@@ -199,15 +234,13 @@ class SeerrService
                     SeerrAvailability.from(tv.mediaInfo?.status)
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(tv.firstAirDate),
-                posterUrl = current.imageUrlBuilder(ImageType.PRIMARY, tv.posterPath),
-                backDropUrl = current.imageUrlBuilder(ImageType.BACKDROP, tv.backdropPath),
+                posterUrl = createImageUrl(ImageType.PRIMARY, tv.posterPath, tv.mediaInfo),
+                backDropUrl = createImageUrl(ImageType.BACKDROP, tv.backdropPath, tv.mediaInfo),
                 jellyfinItemId = tv.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(tv: TvDetails): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(tv: TvDetails): DiscoverItem =
+            DiscoverItem(
                 id = tv.id!!,
                 type = SeerrItemType.TV,
                 title = tv.name,
@@ -217,15 +250,13 @@ class SeerrService
                     SeerrAvailability.from(tv.mediaInfo?.status)
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(tv.firstAirDate),
-                posterUrl = current.imageUrlBuilder(ImageType.PRIMARY, tv.posterPath),
-                backDropUrl = current.imageUrlBuilder(ImageType.BACKDROP, tv.backdropPath),
+                posterUrl = createImageUrl(ImageType.PRIMARY, tv.posterPath, tv.mediaInfo),
+                backDropUrl = createImageUrl(ImageType.BACKDROP, tv.backdropPath, tv.mediaInfo),
                 jellyfinItemId = tv.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(search: SeerrSearchResult): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(search: SeerrSearchResult): DiscoverItem =
+            DiscoverItem(
                 id = search.id,
                 type = SeerrItemType.fromString(search.mediaType),
                 title = search.title ?: search.name,
@@ -235,15 +266,13 @@ class SeerrService
                     SeerrAvailability.from(search.mediaInfo?.status)
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(search.releaseDate ?: search.firstAirDate),
-                posterUrl = current.imageUrlBuilder(ImageType.PRIMARY, search.posterPath),
-                backDropUrl = current.imageUrlBuilder(ImageType.BACKDROP, search.backdropPath),
+                posterUrl = createImageUrl(ImageType.PRIMARY, search.posterPath, search.mediaInfo),
+                backDropUrl = createImageUrl(ImageType.BACKDROP, search.backdropPath, search.mediaInfo),
                 jellyfinItemId = search.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(credit: CreditCast): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(credit: CreditCast): DiscoverItem =
+            DiscoverItem(
                 id = credit.id!!,
                 type = SeerrItemType.fromString(credit.mediaType, SeerrItemType.PERSON),
                 title = credit.name ?: credit.title,
@@ -254,22 +283,22 @@ class SeerrService
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(credit.firstAirDate),
                 posterUrl =
-                    current.imageUrlBuilder(
+                    createImageUrl(
                         ImageType.PRIMARY,
                         credit.posterPath ?: credit.profilePath,
+                        credit.mediaInfo,
                     ),
                 backDropUrl =
-                    current.imageUrlBuilder(
+                    createImageUrl(
                         ImageType.BACKDROP,
                         credit.backdropPath,
+                        credit.mediaInfo,
                     ),
                 jellyfinItemId = credit.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
 
-        suspend fun createDiscoverItem(credit: CreditCrew): DiscoverItem {
-            val current = seerrServerRepository.current.firstOrNull()
-            return DiscoverItem(
+        suspend fun createDiscoverItem(credit: CreditCrew): DiscoverItem =
+            DiscoverItem(
                 id = credit.id!!,
                 type = SeerrItemType.fromString(credit.mediaType, SeerrItemType.PERSON),
                 title = credit.name ?: credit.title,
@@ -280,16 +309,17 @@ class SeerrService
                         ?: SeerrAvailability.UNKNOWN,
                 releaseDate = toLocalDate(credit.firstAirDate),
                 posterUrl =
-                    current.imageUrlBuilder(
+                    createImageUrl(
                         ImageType.PRIMARY,
                         credit.posterPath ?: credit.profilePath,
+                        credit.mediaInfo,
                     ),
                 backDropUrl =
-                    current.imageUrlBuilder(
+                    createImageUrl(
                         ImageType.BACKDROP,
                         credit.backdropPath,
+                        credit.mediaInfo,
                     ),
                 jellyfinItemId = credit.mediaInfo?.jellyfinMediaId?.toUUIDOrNull(),
             )
-        }
     }
