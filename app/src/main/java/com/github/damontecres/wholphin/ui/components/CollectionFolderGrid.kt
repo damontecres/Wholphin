23856ffcf87fbcd85661ac71e61da5f8d1fd89c9
Damontecres <damontecres@gmail.com>
2026-05-25 20:05:44 -0400

    Fixes favorites page not loading (#1462)
    ## Description
    - Fixes a bug introduced in #1453 which broke the favorite page
    - Follow up to #1428 to refactor the collection folder code to hoist
    state up another level
    - Fixes resuming playback from series details play button
    
    ### Related issues
    Fixes #1313
    Fixes #1461
    Follow up to #1453 & #1428 
    
    ### Testing
    Emulator
    
    ## Screenshots
    N/A
    
    ## AI or LLM usage
    None

--- app/src/main/java/com/github/damontecres/wholphin/ui/components/CollectionFolderGrid.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/components/CollectionFolderGrid.kt
@@ -16,21 +16,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.NonRestartableComposable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
@@ -44,17 +36,13 @@ import com.github.damontecres.wholphin.data.model.BaseItem
 import com.github.damontecres.wholphin.data.model.GetItemsFilter
 import com.github.damontecres.wholphin.preferences.UserPreferences
 import com.github.damontecres.wholphin.ui.AspectRatios
-import com.github.damontecres.wholphin.ui.RequestOrRestoreFocus
 import com.github.damontecres.wholphin.ui.cards.GridCard
 import com.github.damontecres.wholphin.ui.data.SortAndDirection
 import com.github.damontecres.wholphin.ui.detail.CardGrid
 import com.github.damontecres.wholphin.ui.detail.GridItemDetails
 import com.github.damontecres.wholphin.ui.main.HomePageHeader
 import com.github.damontecres.wholphin.ui.playback.scale
-import com.github.damontecres.wholphin.ui.rememberInt
-import com.github.damontecres.wholphin.ui.tryRequestFocus
 import com.github.damontecres.wholphin.ui.util.ScrollToTopBringIntoViewSpec
-import com.github.damontecres.wholphin.util.DataLoadingState
 import org.jellyfin.sdk.model.api.BaseItemKind
 import org.jellyfin.sdk.model.api.CollectionType
 import org.jellyfin.sdk.model.api.ItemSortBy
@@ -64,76 +52,25 @@ import kotlin.time.Duration
 @Composable
 fun CollectionFolderGrid(
     preferences: UserPreferences,
-    item: BaseItem?,
-    title: String,
-    items: DataLoadingState<List<BaseItem?>>,
+    collectionType: CollectionType?,
+    focusedItem: BaseItem?,
+    items: List<BaseItem?>,
     sortAndDirection: SortAndDirection,
     onClickItem: (Int, BaseItem) -> Unit,
     onLongClickItem: (Int, BaseItem) -> Unit,
-    onSortChange: (SortAndDirection) -> Unit,
     letterPosition: suspend (Char) -> Int,
-    sortOptions: List<ItemSortBy>,
-    playEnabled: Boolean,
-    getPossibleFilterValues: suspend (ItemFilterBy<*>) -> List<FilterValueOption>,
     viewOptions: ViewOptions,
-    onClickPlayAll: (shuffle: Boolean) -> Unit,
     onClickPlay: (Int, BaseItem) -> Unit,
-    onChangeBackdrop: (BaseItem) -> Unit,
-    onClickShowViewOptions: () -> Unit,
     initialPosition: Int,
+    gridFocusRequester: FocusRequester,
     modifier: Modifier = Modifier,
-    showTitle: Boolean = true,
     positionCallback: ((columns: Int, position: Int) -> Unit)? = null,
-    currentFilter: GetItemsFilter = GetItemsFilter(),
-    filterOptions: List<ItemFilterBy<*>> = listOf(),
-    onFilterChange: (GetItemsFilter) -> Unit = {},
-    focusRequesterOnEmpty: FocusRequester? = null,
 ) {
-    val context = LocalContext.current
-
-    val pager = (items as? DataLoadingState.Success)?.data
-    var showHeader by rememberSaveable { mutableStateOf(true) }
-    val headerRowFocusRequester = remember { FocusRequester() }
-
-    val gridFocusRequester = remember { FocusRequester() }
-    if (pager?.isNotEmpty() == true) {
-        RequestOrRestoreFocus(gridFocusRequester)
-    } else {
-        LaunchedEffect(Unit) {
-            (focusRequesterOnEmpty ?: headerRowFocusRequester).tryRequestFocus()
-        }
-    }
-    var backdropImageUrl by remember { mutableStateOf<String?>(null) }
-
-    var position by rememberInt(initialPosition)
-    val focusedItem = pager?.getOrNull(position)
-    if (viewOptions.showBackdrop) {
-        LaunchedEffect(focusedItem) {
-            focusedItem?.let(onChangeBackdrop)
-        }
-    }
-
     Box(modifier = modifier) {
         Column(
             verticalArrangement = Arrangement.spacedBy(0.dp),
             modifier = Modifier.fillMaxSize(),
         ) {
-            CollectionFolderHeader(
-                showHeader = showHeader || items !is DataLoadingState.Success,
-                showTitle = showTitle,
-                playEnabled = playEnabled && pager?.isNotEmpty() == true,
-                title = title,
-                sortAndDirection = sortAndDirection,
-                onSortChange = onSortChange,
-                sortOptions = sortOptions,
-                getPossibleFilterValues = getPossibleFilterValues,
-                onClickShowViewOptions = onClickShowViewOptions,
-                onClickPlayAll = onClickPlayAll,
-                currentFilter = currentFilter,
-                filterOptions = filterOptions,
-                onFilterChange = onFilterChange,
-                modifier = Modifier.focusRequester(headerRowFocusRequester),
-            )
             val defaultBringIntoViewSpec = LocalBringIntoViewSpec.current
             val density = LocalDensity.current
             AnimatedVisibility(viewOptions.showDetails) {
@@ -147,62 +84,43 @@ fun CollectionFolderGrid(
                             .padding(HeaderUtils.padding),
                 )
             }
-            when (val state = items) {
-                DataLoadingState.Pending,
-                DataLoadingState.Loading,
-                -> {
-                    // This shouldn't happen, so just show placeholder
-                    Text(stringResource(R.string.loading))
-                }
-
-                is DataLoadingState.Error -> {
-                    ErrorMessage(state.message, state.exception)
-                }
-
-                is DataLoadingState.Success<List<BaseItem?>> -> {
-                    CardGrid(
-                        pager = state.data,
-                        onClickItem = onClickItem,
-                        onLongClickItem = onLongClickItem,
-                        onClickPlay = onClickPlay,
-                        letterPosition = letterPosition,
-                        gridFocusRequester = gridFocusRequester,
-                        showJumpButtons = false, // TODO add preference
-                        showLetterButtons = sortAndDirection.sort == ItemSortBy.SORT_NAME,
-                        modifier = Modifier.fillMaxSize(),
-                        initialPosition = initialPosition,
-                        positionCallback = { columns, newPosition ->
-                            showHeader = newPosition < columns
-                            position = newPosition
-                            positionCallback?.invoke(columns, newPosition)
-                        },
-                        cardContent = { (item, index, onClick, onLongClick, widthPx, mod) ->
-                            GridCard(
-                                item = item,
-                                onClick = onClick,
-                                onLongClick = onLongClick,
-                                imageContentScale = viewOptions.contentScale.scale,
-                                imageAspectRatio = viewOptions.aspectRatio.ratio,
-                                imageType = viewOptions.imageType,
-                                showTitle = viewOptions.showTitles,
-                                fillWidth = widthPx,
-                                modifier = mod,
-                            )
-                        },
-                        columns = viewOptions.columns,
-                        spacing = viewOptions.spacing.dp,
-                        bringIntoViewSpec =
-                            remember(viewOptions) {
-                                val spacingPx = with(density) { viewOptions.spacing.dp.toPx() }
-                                if (viewOptions.showDetails) {
-                                    ScrollToTopBringIntoViewSpec(spacingPx)
-                                } else {
-                                    defaultBringIntoViewSpec
-                                }
-                            },
+            CardGrid(
+                pager = items,
+                onClickItem = onClickItem,
+                onLongClickItem = onLongClickItem,
+                onClickPlay = onClickPlay,
+                letterPosition = letterPosition,
+                gridFocusRequester = gridFocusRequester,
+                showJumpButtons = false, // TODO add preference
+                showLetterButtons = sortAndDirection.sort == ItemSortBy.SORT_NAME,
+                modifier = Modifier.fillMaxSize(),
+                initialPosition = initialPosition,
+                positionCallback = positionCallback,
+                cardContent = { (item, index, onClick, onLongClick, widthPx, mod) ->
+                    GridCard(
+                        item = item,
+                        onClick = onClick,
+                        onLongClick = onLongClick,
+                        imageContentScale = viewOptions.contentScale.scale,
+                        imageAspectRatio = viewOptions.aspectRatio.ratio,
+                        imageType = viewOptions.imageType,
+                        showTitle = viewOptions.showTitles,
+                        fillWidth = widthPx,
+                        modifier = mod,
                     )
-                }
-            }
+                },
+                columns = viewOptions.columns,
+                spacing = viewOptions.spacing.dp,
+                bringIntoViewSpec =
+                    remember(viewOptions) {
+                        val spacingPx = with(density) { viewOptions.spacing.dp.toPx() }
+                        if (viewOptions.showDetails) {
+                            ScrollToTopBringIntoViewSpec(spacingPx)
+                        } else {
+                            defaultBringIntoViewSpec
+                        }
+                    },
+            )
         }
     }
 }
