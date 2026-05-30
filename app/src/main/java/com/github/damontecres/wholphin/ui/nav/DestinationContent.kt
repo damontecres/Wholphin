68836bb8346c7f05f12745a063b1053808950ab1
Damontecres <damontecres@gmail.com>
2026-05-30 17:41:27 -0400

    Fix click/long click issues on some view more grid pages (#1486)
    ## Description
    On a library recommended tab, if you clicked View More, clicking a card
    on the grid would directly play the item instead of the regular
    behavior.
    
    This PR fixes that and simplifies the code required to support simple
    context menus.
    
    ### Related issues
    Fixes #1480
    
    ### Testing
    Emulator
    
    ## Screenshots
    N/A
    
    ## AI or LLM usage
    None

--- app/src/main/java/com/github/damontecres/wholphin/ui/nav/DestinationContent.kt
+++ app/src/main/java/com/github/damontecres/wholphin/ui/nav/DestinationContent.kt
@@ -314,6 +314,7 @@ fun DestinationContent(
         is Destination.ItemGrid<*> -> {
             LaunchedEffect(Unit) { onClearBackdrop.invoke() }
             ItemGrid(
+                preferences,
                 destination,
                 modifier,
             )
