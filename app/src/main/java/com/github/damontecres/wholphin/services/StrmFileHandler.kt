b0d9c718fec0ce0bed413b7835025745f0b600e5
Damontecres <damontecres@gmail.com>
2026-07-13 14:11:18 -0400

    Resolve multiple media sources

--- app/src/main/java/com/github/damontecres/wholphin/services/StrmFileHandler.kt
+++ app/src/main/java/com/github/damontecres/wholphin/services/StrmFileHandler.kt
@@ -23,36 +23,45 @@ class StrmFileHandler
         private val api: ApiClient,
     ) {
         /**
-         * Attempts to resolve if the strm file is not yet resolved.
+         * Attempts to resolve a strm file is not yet resolved. This will attempt to resolve every media source.
          *
-         * If the item is not a strm, file, nothing happens
+         * If the item is not a strm file, nothing happens
+         *
+         * @return the resolved item or null if could not be resolved
+         */
+        suspend fun resolveStrm(item: BaseItem): BaseItem? {
+            if (shouldResolveStrm(item)) {
+                Timber.d("Resolving strm %s", item.id)
+                // Resolve the file as a while
+                resolve(item.id, null)
+                val resolvedItem = waitForUpdate(item.id) { shouldResolveStrm(it) }
+                // Resolve each media source that is unresolved
+                resolvedItem.data.mediaSources
+                    .orEmpty()
+                    .filter { it.mediaStreams.isNullOrEmpty() }
+                    .forEach {
+                        resolve(item.id, it.id)
+                    }
+                return waitForUpdate(item.id) { shouldResolveStrm(it) }
+            }
+            return null
+        }
+
+        /**
+         * Attempts to resolve a specific media source in the strm file if is not yet resolved.
+         *
+         * If the item is not a strm file, nothing happens
          *
          * @return the resolved item or null if could not be resolved
          */
         suspend fun resolveStrm(
             item: BaseItem,
-            sourceId: String? = null,
+            sourceId: String,
         ): BaseItem? {
             if (shouldResolveStrm(item, sourceId)) {
                 Timber.d("Resolving strm %s", item.id)
-                resolve(item.id, null)
-                var count = 5
-                while (count > 0) {
-                    Timber.v("Checking for streams in %s", item.id)
-                    val fetchedItem =
-                        api.userLibraryApi
-                            .getItem(item.id)
-                            .content
-                            .let { BaseItem(it) }
-                    if (!shouldResolveStrm(fetchedItem, sourceId)) {
-                        Timber.d("Got updated streams for %s", item.id)
-                        return fetchedItem
-                    }
-                    delay(2.seconds)
-                    count--
-                }
-                Timber.w("Resolve timed out for %s", item.id)
-                throw TimeoutException("Resolve timed out for " + item.id)
+                resolve(item.id, sourceId)
+                return waitForUpdate(item.id) { shouldResolveStrm(it, sourceId) }
             }
             return null
         }
@@ -76,12 +85,44 @@ class StrmFileHandler
             }
         }
 
+        private suspend fun waitForUpdate(
+            itemId: UUID,
+            valid: (BaseItem) -> Boolean,
+        ): BaseItem {
+            var count = 5
+            while (count > 0) {
+                Timber.v("Checking for streams in %s", itemId)
+                val fetchedItem =
+                    api.userLibraryApi
+                        .getItem(itemId)
+                        .content
+                        .let { BaseItem(it) }
+                if (!valid(fetchedItem)) {
+                    Timber.d("Got updated streams for %s", itemId)
+                    return fetchedItem
+                }
+                delay(2.seconds)
+                count--
+            }
+            Timber.w("Resolve timed out for %s", itemId)
+            throw TimeoutException("Resolve timed out for $itemId")
+        }
+
         companion object {
             fun isStrmFile(item: BaseItem): Boolean = item.data.path?.endsWith(".strm") == true
 
+            fun shouldResolveStrm(item: BaseItem): Boolean {
+                if (!isStrmFile(item)) {
+                    return false
+                }
+                return item.data.mediaSources
+                    .orEmpty()
+                    .any { it.mediaStreams.isNullOrEmpty() }
+            }
+
             fun shouldResolveStrm(
                 item: BaseItem,
-                sourceId: String? = null,
+                sourceId: String,
             ): Boolean {
                 if (!isStrmFile(item)) {
                     return false
